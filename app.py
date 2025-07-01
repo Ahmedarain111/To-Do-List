fe4b70788c8fa4b15623cc96d18ed652e61489c0
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-07-01 15:43:46 +0500

    Write Task class

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
+from datetime import datetime
+import os
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+CORS(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'tasks.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(25), nullable=False)
+    desc = db.Column(db.String(60))
+    priority = db.Column(db.String(4), default='low')
+    group = db.Column(db.String(20))
+    date = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return f'<Task {self.id} - {self.title}>'
 
 @app.route('/')
 def main():
     return render_template('main.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
